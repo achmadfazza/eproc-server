b207909929fbcae4ba8a582f1f5df5031a875779
faza <arkadyfaza@gmail.com>
2022-05-19 10:52:01 +0700

    update controller dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,25 @@
+const Category = require('../category/model');
+const Location = require('../location/model');
+const Barang = require('../barang/model');
+const User = require('../users/model');
+
 module.exports = {
 	index: async (req, res) => {
 		try {
+			const category = await Category.countDocuments();
+			const location = await Location.countDocuments();
+			const barang = await Barang.countDocuments();
+			const user = await User.countDocuments();
+
 			res.render('admin/dashboard/view_dashboard', {
 				name: req.session.user.name,
 				title: 'Halaman Dashboard',
+				count: {
+					category,
+					location,
+					barang,
+					user,
+				},
 			});
 		} catch (err) {
 			console.log();
