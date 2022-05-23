67663932251ff4eb4b3df619385e6339ddc5cc51
faza <arkadyfaza@gmail.com>
2022-05-23 13:19:30 +0700

    update controller player

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,4 +1,5 @@
 const Category = require('../category/model');
+const Barang = require('../barang/model');
 
 module.exports = {
 	category: async (req, res) => {
@@ -7,7 +8,16 @@ module.exports = {
 
 			res.status(200).json({ data: category });
 		} catch (err) {
-			req.status(500).json({ message: err.message || 'Internal Server Error' });
+			res.status(500).json({ message: err.message || 'Internal Server Error' });
+		}
+	},
+	landingPage: async (req, res) => {
+		try {
+			const barang = await Barang.findOne({});
+
+			res.status(200).json({ data: barang });
+		} catch (err) {
+			res.status(500).json({ message: err.message || 'Internal Server Error' });
 		}
 	},
 };
--- app/player/router.js
+++ app/player/router.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { category } = require('./controller');
+const { category, landingPage } = require('./controller');
 
 router.get('/category', category);
+router.get('/barang', landingPage);
 
 module.exports = router;
