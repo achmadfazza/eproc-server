cecc07695b31f0fdf5fecd1f7f7ab48546732e4b
faza <arkadyfaza@gmail.com>
2022-05-25 08:11:50 +0700

    add player model

---
+++ app/player/model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+let playerSchema = mongoose.Schema({
+	namaSupplier: {
+		type: String,
+		require: [true, 'nama harus diisi'],
+	},
+	alamat: {
+		type: String,
+		require: [true, 'alamat harus diisi'],
+	},
+	jenisSupplier: {
+		type: String,
+		require: [true, 'jenis supplier harus diisi'],
+	},
+	npwp: {
+		type: String,
+		require: [true, 'npwp harus diisi'],
+	},
+	nik: {
+		type: String,
+		require: [true, 'nik harus diisi'],
+	},
+	noTelp: {
+		type: String,
+		require: [true, 'no telp harus diisi'],
+	},
+	email: {
+		type: String,
+		require: [true, 'email harus diisi'],
+	},
+});
