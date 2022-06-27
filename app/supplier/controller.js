7092c9db05e03710c4582f9fb342293aad0e7592
faza <arkadyfaza@gmail.com>
2022-06-27 14:08:20 +0700

    update controller landingpage

--- app/supplier/controller.js
+++ app/supplier/controller.js
@@ -5,7 +5,7 @@ const Location = require("../location/model");
 module.exports = {
 	landingPage: async (req, res) => {
 		try {
-			const barang = await Barang.find().select("_id name category location reservationdate description");
+			const barang = await Barang.find().select("_id name reservationdate description");
 
 			if (!barang) {
 				return res.status(404).json({ message: "Barang tidak ditemukan.!" });
