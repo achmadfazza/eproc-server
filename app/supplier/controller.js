a130ba861deaf4a21785a8df66a8bf5c67e60151
faza <arkadyfaza@gmail.com>
2022-06-29 10:46:12 +0700

    update controller

--- app/supplier/controller.js
+++ app/supplier/controller.js
@@ -11,7 +11,7 @@ module.exports = {
 				return res.status(404).json({ message: "Barang tidak ditemukan.!" });
 			}
 
-			res.status(200).json({ data: barang });
+			res.status(200).json({ barang });
 		} catch (err) {
 			res.status(500).json({ message: err.message || "Internal Server Error" });
 		}
@@ -25,7 +25,7 @@ module.exports = {
 				return res.status(400).json({ message: "Barang tidak ditemukan" });
 			}
 
-			res.status(200).json({ data: barang });
+			res.status(200).json({ barang });
 		} catch (err) {
 			res.status(500).json({ message: err.message || "Internal server error" });
 		}
@@ -35,7 +35,7 @@ module.exports = {
 		try {
 			const category = await Category.find();
 
-			res.status(200).json({ data: category });
+			res.status(200).json({ category });
 		} catch (err) {
 			res.status(500).json({ message: err.message || "Internal Server Error" });
 		}
@@ -49,7 +49,7 @@ module.exports = {
 				return res.status(404).json({ message: "Barang tidak ditemukan.!" });
 			}
 
-			res.status(200).json({ data: location });
+			res.status(200).json({ location });
 		} catch (err) {
 			res.status(500).json({ message: err.message || "Internal Server Error" });
 		}
